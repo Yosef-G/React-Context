264badfb17bb3571d1f27ad68c3b7c4b3d0bad73
Yosef-G <107163317+Yosef-G@users.noreply.github.com>
2022-09-17 18:38:06 -0400

    Add files via upload

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { MovieContext } from "./MovieContext";
+import "./Nav.css";
+const Nav = () => {
+  const [movies, setMovies] = useContext(MovieContext);
+  return (
+    <div className="nav">
+      <h3>Yosef Gessese</h3>
+      <p>List of Movies:{movies.length}</p>
+    </div>
+  );
+};
+export default Nav;
